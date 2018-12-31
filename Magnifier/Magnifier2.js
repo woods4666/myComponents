1b14a8c7eba9d7ee2932bdedaaca2f0b62e2289f
woods4666 <1219257166@qq.com>
2018-12-31 19:25:53 +0800

    [add]放大镜组件注释

--- Magnifier/Magnifier2.js
+++ Magnifier/Magnifier2.js
@@ -1,6 +1,6 @@
 ﻿/**
  * Created by Administrator on 2018/12/30 0030.
- * @description: 放大镜
+ *@description：放大镜
  */
 import React from 'react';
 import './Magnifier2.less';
@@ -27,53 +27,50 @@ class Magnifier extends React.Component {
   constructor() {
     super();
     this.state = {
-      max: 'width',
-      imgIndex: 0,
-      isEnterSmallBox: false,
-      markPosition: {
+      max: 'width',//图片那条边比较长
+      imgIndex: 0,//当前图片在imgList中的索引
+      isEnterImgBox: false,//鼠标是否进入图片
+      markPosition: {//mark定位
         left: 0,
         top: 0
       }
     };
   }
 
-  adjustImgSize = () => {
-    let {offsetWidth, offsetHeight} = this.smallImg;
-    this.setState({
-      max: offsetWidth >= offsetHeight ? 'width' : 'height'
-    });
-  };
+  // 切换图片
   handleSwitch = () => {
     this.setState(preState => ({
       imgIndex: preState.imgIndex >= imgList.length - 1 ? 0 : preState.imgIndex + 1
     }));
   };
 
+  // 调整大图片和小图片的尺寸
+  adjustImgSize = () => {
+    let {offsetWidth, offsetHeight} = this.smallImg;
+    this.setState({
+      max: offsetWidth >= offsetHeight ? 'width' : 'height'
+    });
+  };
+
+  // 鼠标进入图片时
   enterSmallBox = e => {
-    console.log('enterSmallBox');
     this.setState({
-      isEnterSmallBox: true
+      isEnterImgBox: true
     });
   };
+
+  // 鼠标离开图片时
   leaveSmallBox = e => {
-    console.log('leaveSmallBox');
     this.setState({
-      isEnterSmallBox: false
+      isEnterImgBox: false
     });
   };
-  computedOffset = (node, offsetName) => {
-    let offset = 0,
-      parent = node.offsetParent;
-    while (parent) {
-      offset = offset + node[offsetName];
-      node = parent;
-      parent = node.offsetParent;
-    }
-    return offset;
-  };
+
+  //鼠标在图片中移动
   moveInSmallBox = e => {
     this.setMarkPosition(e);
   };
+  //改变mark的定位
   setMarkPosition = e => {
     let {pageX, pageY} = e,
       {offsetWidth: smallImgW, offsetHeight: smallImgH} = this.smallImg,
@@ -93,63 +90,87 @@ class Magnifier extends React.Component {
       }
     });
   };
+  //计算图片的偏移量（offsetLeft/offsetTop）
+  computedOffset = (node, offsetName) => {
+    let offset = 0,
+      parent = node.offsetParent;
+    while (parent) {
+      offset = offset + node[offsetName];
+      node = parent;
+      parent = node.offsetParent;
+    }
+    return offset;
+  };
 
   render() {
+    //计算样式用到的变量
     let {proportion, proportionSmallBox, magnifierWidth, isCircle, markOpacity} = this.props,
-      {isEnterSmallBox, markPosition} = this.state,
-      display = isEnterSmallBox ? 'block' : 'none',
-      opacity = isEnterSmallBox ? markOpacity : 0,
-      smallBoxWidth = proportionSmallBox * magnifierWidth,
-      smallBoxStyle = {width: smallBoxWidth, height: smallBoxWidth, background: 'yellow'},
-      radiusStyle = isCircle ? {borderRadius: '50%'} : {},
-      bigBoxWidth = (1 - proportionSmallBox) * magnifierWidth,
-      bigBoxStyle = {width: bigBoxWidth, height: bigBoxWidth, display, ...radiusStyle},
-      markWidth = proportion * smallBoxWidth,
+      {isEnterImgBox, markPosition, imgIndex, max} = this.state,
+      {smallImgSrc, bigImgSrc} = imgList[imgIndex],
+      display = isEnterImgBox ? 'block' : 'none',
+      opacity = isEnterImgBox ? markOpacity : 0,
+      radiusStyle = isCircle ? {borderRadius: '50%'} : {};
+
+    //smallBox样式
+    let smallBoxWidth = proportionSmallBox * magnifierWidth,
+      smallBoxStyle = {width: smallBoxWidth, height: smallBoxWidth};
+
+    //mark样式
+    let markWidth = proportion * smallBoxWidth,
       markStyle = {
         width: markWidth,
         height: markWidth,
         opacity,
-        left: markPosition.left,   //动态
-        top: markPosition.top,   //动态
+        left: markPosition.left,
+        top: markPosition.top,
         ...radiusStyle
-      },
-      smallImgStyle = {
-        [this.state.max]: smallBoxWidth
-      },
-      bigImgBoxWidth = bigBoxWidth / proportion,
-      positionProportion = proportion / (1 / proportionSmallBox - 1),
-      bigImgStyle = {
-        [this.state.max]: bigImgBoxWidth,
-        left: -markPosition.left / positionProportion, //动态的
-        top: -markPosition.top / positionProportion //动态的
       };
-    let {imgIndex} = this.state,
-      {smallImgSrc, bigImgSrc} = imgList[imgIndex];
+
+    //smallImg样式
+    let smallImgStyle = {
+      [max]: smallBoxWidth
+    };
+
+    //bigBox样式
+    let bigBoxWidth = (1 - proportionSmallBox) * magnifierWidth,
+      bigBoxStyle = {width: bigBoxWidth, height: bigBoxWidth, display, ...radiusStyle};
+
+    //bigImg样式
+    let bigImgBoxWidth = bigBoxWidth / proportion,
+      positionProportion = proportion / (1 / proportionSmallBox - 1),
+     bigImgStyle = {
+        [max]: bigImgBoxWidth,
+        left: -markPosition.left / positionProportion,
+        top: -markPosition.top / positionProportion
+      };
+
     return (
       [
         <Button type="primary" onClick={this.handleSwitch} key="button">切换图片</Button>,
         <div className="Magnifier2" key="Magnifier2">
           <div className="smallBox" {...{
-            style: {...smallBoxStyle}
+            style: smallBoxStyle
           }}>
             <div className="imgBox" {...{
-              onMouseEnter: this.enterSmallBox,
-              onMouseLeave: this.leaveSmallBox,
-              onMouseMove: this.moveInSmallBox
+              onMouseEnter: this.enterSmallBox,//鼠标进入图片
+              onMouseLeave: this.leaveSmallBox,//鼠标离开图片
+              onMouseMove: this.moveInSmallBox//鼠标在图片中移动
             }}>
               <div className="mark" {...{
-                style: {...markStyle},
+                style: markStyle,
                 ref: node => this.markNode = node
-              }}></div>
+              }} />
               <img {...{
                 src: smallImgSrc,
-                style: {...smallImgStyle},
+                style: smallImgStyle,
                 ref: node => this.smallImg = node,
-                onLoad: this.adjustImgSize,
+                onLoad: this.adjustImgSize,//图片加载完成
               }} alt=""/>
             </div>
           </div>
-          <div className="bigBox" style={{...bigBoxStyle}}>
+          <div className="bigBox" {...{
+            style: bigBoxStyle
+          }}>
             <img {...{
               src: bigImgSrc,
               style: {...bigImgStyle}
@@ -161,10 +182,10 @@ class Magnifier extends React.Component {
   }
 }
 Magnifier.defaultProps = {
-  proportion: 1 / 3,
-  proportionSmallBox: 2 / 5,
-  magnifierWidth: 1000,
-  isCircle: true,
-  markOpacity: .7
+  proportion: 1 / 3, //mark占小盒子宽度的比例
+  proportionSmallBox: 2 / 5,//小盒子占整行宽度的比例
+  magnifierWidth: 1000,//设置整行的宽度
+  isCircle: true,//mark和看到的大图片是否为圆形
+  markOpacity: .7//mark透明度
 };
 export default Magnifier;
